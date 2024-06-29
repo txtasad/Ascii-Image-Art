a2ade8b062cddaa557ede4aa05e7816e4cdd43aa
Mohammad Asad <txtasad@users.noreply.github.com>
2024-06-29 20:48:33 +0530

    init file

---
+++ ascii_image_art/__init__.py
@@ -0,0 +1,10 @@
+#import sys
+#sys.path.append("/Project/src/")
+#__all__ = ['ascii_images_and_text','constants','usingAI']
+#from __future__ import absolute_import
+
+from ascii_image_art.ascii_images_and_text import ASCIIArt
+from ascii_image_art.constants import CHARS_BY_DENSITY
+from ascii_image_art.usingAI import usingAIClass
+
+#hunter#
